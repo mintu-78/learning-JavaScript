307b6205028fcd4fd8f9acb789e023b546e23d94
mintu <pmk80028@gmail.com>
2025-08-19 13:31:04 +0530

    complete guide for learning javaScript

---
+++ index.js
@@ -0,0 +1,17 @@
+const accountId = 3499
+let accountEmail = "mintu@gmail.com"
+var accountPassword = "437894"
+accountCity = "jharkhand"
+
+
+
+accountEmail = "min@gmail.com"
+accountPassword = "84849"
+accountCity = "bihar"
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity])
+
+
+
+
