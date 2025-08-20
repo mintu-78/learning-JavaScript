4fdc561aedb6a25c97bf6efbc495914ef039de1b
mintu <pmk80028@gmail.com>
2025-08-20 23:14:03 +0530

    operation

--- conversion.js
+++ conversion.js
@@ -6,3 +6,27 @@ console.log(typeof(score));
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+
+//*********Operation*********//////
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+
+let str1 = "hitesh"
+let str2 = " hellow"
+let  str3 = str1 + str2 
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+let value2 = 1 + 2 + "2"
+console.log( "typwof", typeof value2);
+
+
+console.log((3 + 4) * 5 % 3);
+
